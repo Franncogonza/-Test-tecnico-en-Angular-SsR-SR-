743e24a747e36a17bfbc483d8240253948432c26
Gonzalez <GFranco@emergencias.com.ar>
2021-03-13 21:15:01 -0300

    clean code

--- src/app/services/license-service/licenses.service.ts
+++ src/app/services/license-service/licenses.service.ts
@@ -1,9 +1,8 @@
 import { map } from 'rxjs/operators';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import * as constants from '../../shared/constants';
-import { LicensesList } from 'src/app/shared/models/licenses-model';
 @Injectable({
   providedIn: 'root'
 })
@@ -14,9 +13,11 @@ export class LicensesService {
 
   constructor(private dataService: HttpClient) { }
 
-  getLicenses(): Observable<LicensesList[]> {
+  getLicenses(){
     const headers = new HttpHeaders({ 'secret-key': this.key });
-    return this.dataService.get<[]>(this.licenses_endpoint, { headers }).pipe(
+
+    return this.dataService.get<[]>(this.licenses_endpoint, { headers })
+    .pipe(
       map((item: any) => item.content));
   }
 
